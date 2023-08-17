64b5a95dc01c8cce877dcb00e2edcf2dead48477
Daniel Vadil <dvadz@yahoo.com>
2023-08-17 17:58:31 -0400

    Created boilerplates for components that will be needed and added them to App'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import CarForm from "./components/CarForm";
+import CarSearch from "./components/CarSearch";
+import CarList from "./components/CarList";
+import CarValues from "./components/CarValues";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <CarForm></CarForm>
+      <CarSearch></CarSearch>
+      <CarList></CarList>
+      <CarValues></CarValues>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/CarValues.js
@@ -0,0 +1,5 @@
+const CarValues = () => {
+  return <div>CarValues</div>;
+};
+
+export default CarValues;
