bb03a0a7f4ba96dda96e989b6641642a01194a1c
Daniel Vadil <dvadz@yahoo.com>
2023-08-20 23:10:34 -0400

    Dispatched changeName when input changes and re-assign back the value

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -3,11 +3,21 @@ import { changeName } from "../store";
 
 const CarForm = () => {
   const dispatch = useDispatch();
+
+  const { name } = useSelector((state) => {
+    return state.form;
+  });
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    dispatch(changeName(event.target.value));
+  };
+
   return (
     <div>
       <form>
         <label>Name</label>
-        <input></input>
+        <input onChange={handleNameChange} value={name}></input>
       </form>
     </div>
   );
