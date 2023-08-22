a7eee05394e5a01a89005d74c6ac9572628c0cc9
Daniel Vadil <dvadz@yahoo.com>
2023-08-22 14:44:25 -0400

    Started working on the CarList, retrieved the list of cars from the store

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,5 +1,11 @@
+import { useSelector } from "react-redux";
+
 const CarList = () => {
-  return <div>CarList</div>;
+  const cars = useSelector((state) => {
+    return state.cars.data;
+  });
+
+  return <div></div>;
 };
 
 export default CarList;
