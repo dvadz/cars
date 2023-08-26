4146e938fe3ca4b37cbe329d9271d48ae896e3e5
Daniel Vadil <dvadz@yahoo.com>
2023-08-25 22:18:24 -0400

    Built the CarSearch component, added input and update state, then retrieved state and assigned to value of the input

--- src/components/CarSearch.js
+++ src/components/CarSearch.js
@@ -1,5 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import { changeSearchTerm } from "../store";
+
 const CarSearch = () => {
-  return <div>CarSearch</div>;
+  const dispatch = useDispatch();
+
+  const handleSearchTermChange = (event) => {
+    dispatch(changeSearchTerm(event.target.value));
+  };
+
+  const searchTerm = useSelector((state) => {
+    return state.cars.searchTerm;
+  });
+
+  return (
+    <div className="list-header">
+      <h3>My Cars</h3>
+      <div className="search field is-horizontal">
+        <label className="label">Search</label>
+        <input
+          className="input"
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+        ></input>
+      </div>
+    </div>
+  );
 };
 
 export default CarSearch;
