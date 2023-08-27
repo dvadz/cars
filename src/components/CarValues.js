895d0f100b815fa49b4196703f34d6746cebc19b
Daniel Vadil <dvadz@yahoo.com>
2023-08-26 23:25:09 -0400

    Implemented CarValue component, same with CarList, filtered list of cars based on seartchTerm, then added up the total cost of filtered cars

--- src/components/CarValues.js
+++ src/components/CarValues.js
@@ -1,5 +1,17 @@
+import { useSelector } from "react-redux";
+
 const CarValues = () => {
-  return <div>CarValues</div>;
+  const totalCost = useSelector(({ cars: { searchTerm, data } }) => {
+    return data
+      .filter((car) => {
+        return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+      })
+      .reduce((acc, car) => {
+        return acc + car.cost;
+      }, 0);
+  });
+
+  return <div className="car-value"> Total Cost: ${totalCost}</div>;
 };
 
 export default CarValues;
